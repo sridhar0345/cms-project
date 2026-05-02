c3954263d43572213767526046d65c2decd4573e
sridhar0345 <sridharsridhar0345@gmail.com>
2026-05-02 17:07:00 +0530

    add oauth2 and logging code

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,19 +1,24 @@
-"""
-The flask application package.
-"""
 import logging
 from flask import Flask
-from config import Config
+from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_session import Session
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# TODO: Add any logging levels and handlers with app.logger
-Session(app)
+
+# Set up logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s %(message)s'
+)
+logger = logging.getLogger(__name__)
+
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
 
-import FlaskWebProject.views
+Session(app)
+
+from FlaskWebProject import views
